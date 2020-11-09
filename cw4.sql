0884707ffe7c1d0cf3ff228962abcfcc6d02aa9b
milenagoluch <61710617+milenagoluch@users.noreply.github.com>
2020-11-09 13:29:23 +0100

    Add files via upload

---
+++ cw4.sql
@@ -0,0 +1,20 @@
+CREATE EXTENSION postgis;
+CREATE TABLE obiekty (id INTEGER, nazwa VARCHAR, geometria GEOMETRY);
+
+INSERT INTO obiekty VALUES (1, 'obiekt1', ST_GeomFromText('MULTICURVE((0 1, 1 1), CIRCULARSTRING(1 1, 2 0, 3 1), CIRCULARSTRING(3 1, 4 2, 5 1), (5 1, 6 1))', 0) );
+INSERT INTO obiekty VALUES (2, 'obiekt2', ST_GeomFromText('CURVEPOLYGON(COMPOUNDCURVE((10 6, 14 6), CIRCULARSTRING(14 6, 16 4, 14 2, 12 0, 10 2), (10 2, 10 6)), CIRCULARSTRING(11 2, 13 2, 11 2) )', 0) );
+INSERT INTO obiekty VALUES (3, 'obiekt3', ST_GeomFromText('POLYGON((10 17, 12 13, 7 15, 10 17))', 0) );
+INSERT INTO obiekty VALUES (4, 'obiekt4', ST_GeomFromText('LINESTRING(20 20, 25 25, 27 24, 25 22, 26 21, 22 19, 20.5 19.5)', 0) );
+INSERT INTO obiekty VALUES (5, 'obiekt5', ST_GeomFromText('MULTIPOINT Z((30 30 59), (38 32 234))',0) );
+INSERT INTO obiekty VALUES (6, 'obiekt6', ST_GeomFromText('GEOMETRYCOLLECTION(LINESTRING(1 1, 3 2), POINT(4 2))',0) );
+SELECT * FROM obiekty;
+
+--1)
+SELECT ST_Area(ST_Buffer(ST_ShortestLine((SELECT geometria FROM obiekty WHERE nazwa ='obiekt3'), (SELECT geometria FROM obiekty WHERE nazwa ='obiekt4')), 5)) AS Pole_powierzchni_bufora;
+--2)
+--Warunkiem jest bycie obiektem zamkniętym, nie linią.
+UPDATE obiekty SET geometria = ST_GeomFromText('POLYGON((20 20, 25 25, 27 24, 25 22, 26 21, 22 19, 20.5 19.5, 20 20))', 0) WHERE nazwa = 'obiekt4';
+--3)
+INSERT INTO obiekty VALUES (7, 'obiekt7', (SELECT ST_Union((SELECT geometria FROM obiekty WHERE nazwa ='obiekt3'), (SELECT geometria FROM obiekty WHERE nazwa ='obiekt4'))));
+--4)
+SELECT SUM (ST_Area(ST_Buffer(geometria, 5))) AS Pole_bufora_bez_lukow FROM obiekty WHERE ST_HasArc(geometria) = false;
